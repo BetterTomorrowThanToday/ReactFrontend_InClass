ef823d73953422db43865905b2ee7ff1c3ac42c7
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2024-11-29 16:17:39 +0900

    fix: 오타 수정

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -4,7 +4,7 @@ const API_URL = process.env.REACT_APP_ALB_URL;
 
 export const fetchUsers = async () => {
     try {
-        const response = await axios.get(API_ㅋURL);
+        const response = await axios.get(API_URL);
         return response.data;
     } catch (error) {
         console.error("Error fetching users:", error);
