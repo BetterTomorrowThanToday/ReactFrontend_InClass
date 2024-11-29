56fd196a4f4351fd6bc7d718d3cc1ea0052a84aa
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2024-11-29 16:08:21 +0900

    fix: url to secret

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/users';
+const API_URL = process.env.REACT_APP_ALB_URL;
 
 export const fetchUsers = async () => {
     try {
-        const response = await axios.get(API_URL);
+        const response = await axios.get(API_ㅋURL);
         return response.data;
     } catch (error) {
         console.error("Error fetching users:", error);
